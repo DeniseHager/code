09daa3d61a4cf380f9d9e3428e3b122e25a63f10
DeniseHager <denise.hager@mohawkcollege.ca>
2023-11-21 12:53:30 -0500

    Add Cisco DevNet modified code

---
+++ NetconfDemo.py
@@ -0,0 +1,18 @@
+"""
+This script automates the code found int he 'Breaking down NETCONF communictions in the Cisco DevNet lab
+Please use the env_lab file provided by your instructor.
+"""
+from ncclient import manager
+from prep import env_lab
+NCManager=manager.connect(
+    host=env_lab.IOS_XE_1["host"],
+    port=env_lab.IOS_XE_1["netconf_port"],
+    username=env_lab.IOS_XE_1["username"],
+    password=env_lab.IOS_XE_1["password"],
+    hostkey_verify=False
+)
+
+for capability in NCManager.server_capabilities:
+    print(capability)
+
+NCManager.close_session()
